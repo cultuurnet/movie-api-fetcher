fe6116fe9f0ecf226055c5416f430a6475002687
Jonas Verhaeghe <jonas@cultuurnet>
2018-03-21 16:08:49 +0100

    Add tests

---
+++ tests/Term/TermFactoryTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace CultuurNet\MovieApiFetcher\Term;
+
+use ValueObjects\Identity\UUID;
+
+class TermFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var TermFactory
+     */
+    private $termFactory;
+
+    protected function setUp()
+    {
+        $terms = array(
+            636 => '1.7.6.0.0',
+            634 => '1.7.7.0.0',
+            635 => '1.7.15.0.0',
+        );
+
+        $this->termFactory = new TermFactory($terms);
+    }
+
+
+
+    /**
+     * @test
+     */
+    public function testTermMapping()
+    {
+        $expected = '1.7.7.0.0';
+        $this->assertEquals($expected, $this->termFactory->mapTerm(634));
+    }
+}
