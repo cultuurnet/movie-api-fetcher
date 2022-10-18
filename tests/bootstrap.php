25088c9953bda9b9c4eb6c3abda435af9b2e5ce3
Jonas <jonas@cultuurnet.be>
2022-10-18 12:52:11 +0200

    update test files

---
+++ tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+class_alias(TestCase::class, 'PHPUnit_Framework_TestCase');
